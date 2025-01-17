2d400b79ce7ed2e4b8ec16c5cbfa45399f045657
Oliver Atherton <118294630+olivrrcode@users.noreply.github.com>
2025-01-17 10:39:35 +0000

    chore: init

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as passport from 'passport';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
+
+  app.use(helmet());
+
   app.enableCors({
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST'],
